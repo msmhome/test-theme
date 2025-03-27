aeb1ede0b5e30d52d73fbe5821b14f4f2557fd7b
msmhome <143322410+msmhome@users.noreply.github.com>
2025-03-27 09:53:24 -0400

    footer edit 3

--- themes/test-theme/views/components/footer.blade.php
+++ themes/test-theme/views/components/footer.blade.php
@@ -1,4 +1,4 @@
 <footer style="margin: 25px 25px 0px 25px;" class="pt-5 pb-3 mt-auto">
     <div class="float-left"><a href="https://fastcat.co">FastCat Hosting &copy; {{ date('Y') }}</a></div>
-    <div class="float-right"><a style="margin: 10px;" href="https://github.com/Sonic-Hedgehog/Voslin-Theme">Powered by Voslin</a> & <a href="https://paymenter.org">Paymenter &copy;</a></div>
+    <div class="float-right"><a style="margin: 10px;" href="https://github.com/Sonic-Hedgehog/Voslin-Theme">Powered by Voslin</a>& <a href="https://paymenter.org">Paymenter</a></div>
 </footer>
